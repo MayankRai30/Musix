039435e9b3acf94c793fb7cff5aff9bd10053f50
MayankRai30 <102607416+MayankRai30@users.noreply.github.com>
2023-10-23 15:09:53 +0530

    line 47 repo = Repo()

--- YukkiMusic/core/git.py
+++ YukkiMusic/core/git.py
@@ -44,7 +44,7 @@ def git():
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
-        repo = Repo()
+       
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
         LOGGER(__name__).info(f"Invalid Git Command")
